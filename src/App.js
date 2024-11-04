276ed5f7d9909474372354215fc6d539b3fabfb3
Sungwoo97 <gwaja97@gmail.com>
2024-11-04 10:24:14 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import BoardList from './BoardList';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React Board</h1>
+      <BoardList/>
+    </div>
+  );
+}
+
+export default App;
